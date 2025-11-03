0e3e59c2f825cbd8f2e4363286f003586fbd051f
martin-rog001 <martin.rog001@gmail.com>
2025-11-03 11:32:33 +0000

    init sentences tables

---
+++ mysql/create_db/init-table.sql
@@ -0,0 +1,7 @@
+CREATE table sentences (
+    sentence_id int NOT NULL,
+    sentence_text MEDIUMTEXT,
+    PRIMARY KEY (sentence_id)
+);
+
+insert into sentences (sentence_id, sentence_text) value (1,'wdwedwed');
